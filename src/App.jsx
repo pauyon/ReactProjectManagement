4ec6758279c08de06677fd0ea4ce439c4dad502f
Paulino Auyon <pauyon@jgboswell.com>
2025-06-04 14:26:38 -0700

    Use state to show/hide new project/no project components

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,35 @@
+import { useState } from "react";
 import NewProject from "./components/NewProject.jsx";
 import NoProjectSelected from "./components/NoProjectSelected.jsx";
 import ProjectsSidebar from "./components/ProjectsSidebar.jsx";
 
 function App() {
+  const [projectsState, setProjectsState] = useState({
+    selectedPojectId: undefined,
+    projects: [],
+  });
+
+  function handleStartAddProject() {
+    setProjectsState(prevState => {
+      return {
+        ...prevState,
+        selectedPojectId: null,
+      };
+    });
+  }
+
+  let content;
+
+  if (projectsState.selectedPojectId === null) {
+    content = <NewProject />;
+  } else if (projectsState.selectedPojectId === undefined) {
+    content = <NoProjectSelected onStartAddProject={handleStartAddProject} />;
+  }
+
   return (
     <main className="h-screen my-8 flex gap-8">
-      <ProjectsSidebar />
-      <NoProjectSelected />
+      <ProjectsSidebar onStartAddProject={handleStartAddProject} />
+      {content}
     </main>
   );
 }
